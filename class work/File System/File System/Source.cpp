015a9d37adc8f16385d2eddb2067590b6966d596
Roma Zaluzhny <Zaluzhny@rivne.itstep.org>
2020-01-16 14:54:36 +0200

    file

---
+++ class work/File System/File System/Source.cpp
@@ -0,0 +1,112 @@
+﻿//
+//Завдання 3.
+//Створити програму, яка працює подібно стандартній програмі Командний Рядок.Тобто
+//програма очікує команду і виконує її.Вихід з програми виконувати за командою exit або
+//quit.Реалізувати наступні команди :
+//o create filename(створення текстового файлу, завершення вводу – комбінація Ctrl - Z)
+//o print filename(вивід текстового файлу на екран)
+//o append filename(доповнення текстового файлу, завершення вводу – комбінація Ctrl -
+//	Z)
+//	o copy filename1 filename2(копіювання файлу)
+//	При введенні невірної команди або вказанні невірного шляху до файлу програма
+//	повинна видавати відповідне повідомлення(«Невірна команда», «Невірна кількість
+//		параметрів», «Невірний шлях до файлу»).
+
+#include <iostream>
+#include <string>
+#include<conio.h>
+#include <fstream>
+
+using namespace std;
+void Create();
+void print();
+void append();
+void Copy();
+
+namespace for_file {
+	string path;
+	string file;
+};
+
+void Stroka() {
+	string comand="";
+	for (int i = 0; comand!="exit";i++) {
+		cout << "\\root: ";
+		cin >> comand;
+		if (comand == "create")			{ Create();}
+		else if (comand == "print")		{ print();}
+		else if (comand == "append")	{ append(); }
+		else if (comand == "copy")		{ Copy(); }
+		else if (comand == "help")		{cout << "create	print	append	copy	exit" << endl; }
+		else if (comand == "exit")		{}
+		else							{cout << "wrong command" << endl;}
+	}
+}
+void Create() {
+	ofstream file;
+	system("cls");
+	cout << "\\root: create ";
+	cin >> for_file::path;
+	string text;
+	file.open(for_file::path, ofstream::app);
+	if (!file.is_open()) {
+		cout << "Can't open file!" << endl;
+	}
+	else {
+			cout << "Enter text" << endl;
+			cin.ignore();
+			getline(cin, text);
+			file << text;
+	}
+	file.close();
+}
+void print() {
+	ifstream file;
+	string read;
+	system("cls");
+	cout << "\\root: print ";
+	cin >> for_file::path;
+	file.open(for_file::path);
+	while (!file.eof()) {
+		getline(file, read);
+		cout << read << endl;
+	}
+	file.close();
+}
+void append() {
+	ofstream file;
+	system("cls");
+	cout << "\\root: append ";
+	cin >> for_file::path;
+	string text;
+	file.open(for_file::path, ofstream::app);
+	if (!file.is_open()) {
+		cout << "Can't open file!" << endl;
+	}
+	else {
+		cout << "Enter text" << endl;
+		cin.ignore();
+		getline(cin, text);
+		file << text;
+	}
+	file.close();
+}
+void Copy() {
+	string name;
+	system("cls");
+	cout << "\\root: copy ";
+	cin >> for_file::path;
+	system("cls");
+	cout << "\\root: copy "<< for_file::path<<" ";
+	cin >> name;
+	ifstream file(for_file::path);
+	getline(file, for_file::file);
+	ofstream to_file(name);
+	to_file << for_file::file;
+}
+
+int main() {
+	Stroka();
+	system("pause");
+	return 0;
+}
--- class work/Game TRIO/Game TRIO/Source.cpp
+++ class work/Game TRIO/Game TRIO/Source.cpp
@@ -15,6 +15,90 @@
 //
 //
 
+//#include <iostream>
+//#include <string>
+//#include <ctime>
+//
+//using namespace std;
+//
+//class unit
+//{
+//public:
+//	string name;
+//	int health;
+//	int damage;
+//	int chans;
+//	void war(unit bad_man);
+//	void show(unit bad_man);
+//	unit() {};
+//	~unit() {};
+//};
+//void unit::war(unit bad_man)
+//{
+//	this->show(bad_man);
+//	srand(unsigned(time(NULL)));
+//	if (rand() % 100 > bad_man.chans) {
+//		bad_man.health = bad_man.health - this->damage;
+//		cout << this->name << " attacked " << bad_man.name << endl;
+//		cout << "Health " << bad_man.name << ": " << bad_man.health << endl;
+//	}
+//	if (rand() % 100 > this->chans) {
+//			this->health = this->health - bad_man.damage;
+//			cout << bad_man.name << " attacked " << this->name << endl;
+//			cout << "Health " << this->name << ": " << this->health << endl;
+//	}
+//	
+//}
+//void unit::show(unit bad_man)
+//{
+//	cout << this->name <<" Health: " << this->health << endl;
+//	cout << bad_man.name<<" Health: " << bad_man.health << endl;
+//}
+//
+//
+//class Swords :public unit
+//{
+//public:
+//	Swords() {
+//		this->name = "Swords";
+//		this->damage = 5;
+//		this->health = 15;
+//		this->chans = 60;
+//	};
+//	~Swords() {};
+//};
+//class Archer :public unit
+//{
+//public:
+//	Archer() {
+//		this->name = "Archer";
+//		this->damage = 10;
+//		this->health = 8;
+//		this->chans = 30;
+//	};
+//	~Archer() {};
+//};
+//class Withard :public unit
+//{
+//public:
+//	Withard() {
+//		this->name = "Withard";
+//		this->damage = 4;
+//		this->health = 12;
+//		this->chans = 40;
+//	};
+//	~Withard() {};
+//};
+//
+//int main() {
+//	Swords x;
+//	Archer y;
+//	x.war(y);
+//
+//	system("pause");
+//return 0;
+//}
+
 #include <iostream>
 #include <string>
 #include <ctime>
@@ -28,33 +112,11 @@ public:
 	int health;
 	int damage;
 	int chans;
-	void war(unit bad_man);
-	void show(unit bad_man);
+	static void war(unit team_one, unit team_two);
+	static void show(unit team_one, unit team_two);
 	unit() {};
 	~unit() {};
 };
-void unit::war(unit bad_man)
-{
-	this->show(bad_man);
-	srand(unsigned(time(NULL)));
-	if (rand() % 100 > bad_man.chans) {
-		bad_man.health = bad_man.health - this->damage;
-		cout << this->name << " attacked " << bad_man.name << endl;
-		cout << "Health " << bad_man.name << ": " << bad_man.health << endl;
-	}
-	if (rand() % 100 > this->chans) {
-			this->health = this->health - bad_man.damage;
-			cout << bad_man.name << " attacked " << this->name << endl;
-			cout << "Health " << this->name << ": " << this->health << endl;
-	}
-	
-}
-void unit::show(unit bad_man)
-{
-	cout << this->name <<" Health: " << this->health << endl;
-	cout << bad_man.name<<" Health: " << bad_man.health << endl;
-}
-
 
 class Swords :public unit
 {
@@ -90,11 +152,62 @@ public:
 	~Withard() {};
 };
 
-int main() {
-	Swords x;
-	Archer y;
-	x.war(y);
+void unit::show(unit team_one, unit team_two)
+{
+	cout << team_two.name << " Health: " << team_two.health << endl;
+	cout << team_one.name << " Health: " << team_one.health << endl;
+}
 
+void unit::war(unit  team_one, unit  team_two)
+{
+	show(team_one, team_two);
+	cout << "--------------------------" << endl;
+	if (rand() % 100 > team_one.chans) {
+		team_one.health = team_one.health - team_two.damage;
+		cout << team_two.name << " attacked " << team_one.name << endl;
+		cout << "Health " << team_one.name << ": " << team_one.health << endl;
+	}
+
+	if (rand() % 100 > team_two.chans) {
+		team_two.health = team_two.health - team_one.damage;
+		cout << team_one.name << " attacked " << team_two.name << endl;
+		cout << "Health " << team_two.name << ": " << team_two.health << endl;
+	}
+}
+
+void Fill_army(unit* army, int size) {
+	Swords swords;
+	Archer archer;
+	Withard withard;
+	int chans;
+	for (int i = 0; i < size; i++) {
+		chans = rand() % 3;
+		if (chans == 1) {
+			army[i]=Swords();
+		}
+		else if (chans == 2) {
+			army[i]=Archer();
+		}
+		else {
+			army[i]=Withard();
+		}
+	}
+	for (int i = 0; i < size; i++) {
+		cout << i + 1 << army[i].name << endl;
+	}
+}
+int main() {
+	srand(unsigned(time(NULL)));
+	const int size = 3;
+	unit* orc=new unit[size];
+	unit *man=new unit[size];
+	cout << "`````````````````````````Team 1``````````````````" << endl;
+	Fill_army(orc, size);
+	cout << "`````````````````````````Team 2``````````````````" << endl;
+	Fill_army(man, size);
+	cout << "`````````````````````````war``````````````````" << endl;
+	unit::war(man[0],orc[0]);
+	
 	system("pause");
-return 0;
+	return 0;
 }
